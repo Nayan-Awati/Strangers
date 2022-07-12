90bea073a60ab9f93a74a3dece2eeed659ad3222
Nayan Jitendra Awati <nayan.awati@gmail.com>
2022-07-12 19:13:32 +0300

    "google authentication enabled"

--- app/src/main/java/com/nayan/strangers/LoginActivity.kt
+++ app/src/main/java/com/nayan/strangers/LoginActivity.kt
@@ -1,17 +1,74 @@
 package com.nayan.strangers
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
+
+    companion object{
+        private const val RC_SIGN_IN = 11
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this@LoginActivity, gso)
+
+
+
+
         findViewById<LinearLayout>(R.id.btn_login).setOnClickListener {
-            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-            finish()
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, RC_SIGN_IN)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode ==  RC_SIGN_IN){
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account: GoogleSignInAccount = task.result
+            authWithGoogle(account.idToken.toString())
+
+        }
+    }
+
+    private fun authWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    val user: FirebaseUser? = auth.currentUser
+                    Log.e("profile", user?.photoUrl.toString())
+                }else{
+                    Log.e("error", it.exception.toString())
+                }
+            }
+    }
+
+
+
 }
